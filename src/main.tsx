09b635c7735f116a78f24efd08fcf9798b5ccadf
Dũng <61448424+madness1997-gif@users.noreply.github.com>
2026-08-05 20:12:06 +0700

    Hide scrollbars in small text boxes

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Home from "../app/page";
 import "../app/globals.css";
+import "../app/textbox-fixes.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
