8cdff716d371e09468bff48e2621bd64ee0f781b
Dũng <61448424+madness1997-gif@users.noreply.github.com>
2026-08-05 21:03:17 +0700

    Enable advanced equation composer

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Home from "../app/page";
+import EquationComposer from "../app/equation-composer";
 import "../app/globals.css";
 import "../app/textbox-fixes.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Home />
+    <EquationComposer />
   </React.StrictMode>,
 );
