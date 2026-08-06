84e143f50b747ac621a6a891ac5d7b92b3f8dd02
Dũng <61448424+madness1997-gif@users.noreply.github.com>
2026-08-07 06:58:29 +0700

    Enable multi-sheet Pages

--- app/independent-library-runtime.ts
+++ app/independent-library-runtime.ts
@@ -4,5 +4,6 @@ void import("./independent-library-ui");
 void import("./relation-note-sidebar")
   .then(() => import("./relation-note-hide-native-thumbnails"))
   .then(() => import("./relation-note-right-layout"))
-  .then(() => import("./relation-navigation-collapse"));
+  .then(() => import("./relation-navigation-collapse"))
+  .then(() => import("./page-sheet-runtime"));
 export {};
