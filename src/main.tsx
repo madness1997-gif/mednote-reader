019542ddb2a9c161de089fb7301646a655d3f7b2
Dũng <61448424+madness1997-gif@users.noreply.github.com>
2026-08-06 12:10:08 +0700

    Enable Note PDF export

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Home from "../app/page";
 import EquationComposer from "../app/equation-composer";
+import NotePdfExporter from "../app/note-pdf-export";
 import "../app/globals.css";
 import "../app/textbox-fixes.css";
 import "../app/note-zoom-fixes.css";
+import "../app/note-pdf-export.css";
 import "../app/note-zoom-runtime";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Home />
     <EquationComposer />
+    <NotePdfExporter />
   </React.StrictMode>,
 );
