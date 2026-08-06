b9409ea871afca7cd81e9ace611a84ba14d19b63
Dũng <61448424+madness1997-gif@users.noreply.github.com>
2026-08-06 11:58:00 +0700

    Fix Note zoom on web and Windows

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,8 @@ import Home from "../app/page";
 import EquationComposer from "../app/equation-composer";
 import "../app/globals.css";
 import "../app/textbox-fixes.css";
+import "../app/note-zoom-fixes.css";
+import "../app/note-zoom-runtime";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
