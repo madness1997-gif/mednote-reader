d485db7eb885d1fb9fd48927757eda177d083c42
Dũng <61448424+madness1997-gif@users.noreply.github.com>
2026-08-06 12:26:56 +0700

    Enable common icon and emoji library in notes

--- src/main.tsx
+++ src/main.tsx
@@ -7,8 +7,10 @@ import "../app/globals.css";
 import "../app/textbox-fixes.css";
 import "../app/note-zoom-fixes.css";
 import "../app/note-pdf-export.css";
+import "../app/note-symbol-library.css";
 import "../app/note-zoom-runtime";
 import "../app/pdf-wheel-zoom-runtime";
+import "../app/note-symbol-library";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
