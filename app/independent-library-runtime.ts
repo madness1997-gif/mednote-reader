61064065bae237a06116ad13c52651a75e5c6c40
Dũng <61448424+madness1997-gif@users.noreply.github.com>
2026-08-06 17:52:28 +0700

    Mount note navigation in the right grid column

--- app/independent-library-runtime.ts
+++ app/independent-library-runtime.ts
@@ -1,4 +1,6 @@
 import "./independent-library-repair";
 void import("./independent-library-ui");
-void import("./relation-note-sidebar").then(() => import("./relation-note-hide-native-thumbnails"));
+void import("./relation-note-sidebar")
+  .then(() => import("./relation-note-hide-native-thumbnails"))
+  .then(() => import("./relation-note-right-layout"));
 export {};
