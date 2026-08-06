073801031a80c8f42f7a171f1fd115b5e2bbe4ff
Dũng <61448424+madness1997-gif@users.noreply.github.com>
2026-08-06 20:17:58 +0700

    Mount navigation hide controls

--- app/independent-library-runtime.ts
+++ app/independent-library-runtime.ts
@@ -2,5 +2,6 @@ import "./independent-library-repair";
 void import("./independent-library-ui");
 void import("./relation-note-sidebar")
   .then(() => import("./relation-note-hide-native-thumbnails"))
-  .then(() => import("./relation-note-right-layout"));
+  .then(() => import("./relation-note-right-layout"))
+  .then(() => import("./relation-navigation-collapse"));
 export {};
